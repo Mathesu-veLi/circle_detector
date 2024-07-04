18b08acc0a6461568635c39c2c7eac1c0d9aa53f
Matheus <matheuslevit@gmail.com>
2024-07-04 16:53:14 -0300

    draws the circles in the frame

--- main.py
+++ main.py
@@ -9,6 +9,15 @@ while True:
     
     gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
     gray_blurred = cv.medianBlur(gray, 5)
+    circles = cv.HoughCircles(gray_blurred, 
+                               cv.HOUGH_GRADIENT, dp=1.2, minDist=100,
+                               param1=100, param2=30, minRadius=50, maxRadius=100)
+    if circles is not None:
+        circles = np.round(circles[0, :]).astype("int")
+        for (x, y, r) in circles:
+            cv.circle(frame, (x, y), r, (255, 0, 255), 3)
+            cv.circle(frame, (x, y), 1, (0, 100, 100), 3)
+    
     cv.imshow("frame", frame)
     if cv.waitKey(1) & 0xFF == ord('q'): break
 
